422512ef6274567cc358e9688bfd79a3bab6ca1f
Joe <joew@fnvi.co.uk>
2016-05-17 13:34:21 +0100

    Created skeleton of package

---
+++ src/Collections/Events.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FNVi\Events\Collections;
+use FNVi\Mongo\Collection;
+/**
+ * Description of Events
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class Events extends Collection{
+    //put your code here
+}
---
+++ src/Schemas/Event.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FNVi\Events\Schemas;
+use FNVi\Mongo\Schema;
+/**
+ * Description of Event
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class Event extends Schema{
+    //put your code here
+}
